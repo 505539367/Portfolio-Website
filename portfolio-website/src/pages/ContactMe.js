0ebcb0676e33e7b6267e73418718ed137d52a7dd
Ying Yang <yyang005@citymail.cuny.edu>
2022-09-05 16:29:45 -0400

    update

--- portfolio-website/src/pages/ContactMe.js
+++ portfolio-website/src/pages/ContactMe.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Contact from '../components/contact/contact'
 import Header from '../components/header/header'
-import Footer from '../components/footer/footer'
 import './page.css'
 
 function ContactMe () {
